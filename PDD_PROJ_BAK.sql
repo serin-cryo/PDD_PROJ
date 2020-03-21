cbe5a89b017d0b8ed1fe7e9a11a60465745a774c
Savio Sajan <saviosajan14@gmail.com>
2020-03-21 21:35:17 +0530

    git is commiting

---
+++ PDD_PROJ_BAK.sql
@@ -0,0 +1,132 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: PDD_PROJ
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `DISTRIBUTOR`
+--
+
+DROP TABLE IF EXISTS `DISTRIBUTOR`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DISTRIBUTOR` (
+  `DID` int(11) DEFAULT NULL,
+  `D_NAME` varchar(15) DEFAULT NULL,
+  `MOB_NO` int(10) DEFAULT NULL,
+  `LOCATION` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DISTRIBUTOR`
+--
+
+LOCK TABLES `DISTRIBUTOR` WRITE;
+/*!40000 ALTER TABLE `DISTRIBUTOR` DISABLE KEYS */;
+INSERT INTO `DISTRIBUTOR` VALUES (5001,'RAKESH',989898989,'THRISSUR');
+/*!40000 ALTER TABLE `DISTRIBUTOR` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EMPLOYEE`
+--
+
+DROP TABLE IF EXISTS `EMPLOYEE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EMPLOYEE` (
+  `EMP_ID` int(11) DEFAULT NULL,
+  `EMP_NA` varchar(15) DEFAULT NULL,
+  `MOB_NO` int(11) DEFAULT NULL,
+  `DEPT` varchar(15) DEFAULT NULL,
+  `SALARY` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EMPLOYEE`
+--
+
+LOCK TABLES `EMPLOYEE` WRITE;
+/*!40000 ALTER TABLE `EMPLOYEE` DISABLE KEYS */;
+INSERT INTO `EMPLOYEE` VALUES (8001,'MEENAKSHI',787878787,'food',8000);
+/*!40000 ALTER TABLE `EMPLOYEE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PRODUCT`
+--
+
+DROP TABLE IF EXISTS `PRODUCT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PRODUCT` (
+  `PID` int(11) DEFAULT NULL,
+  `P_NAME` varchar(15) DEFAULT NULL,
+  `P_TYPE` varchar(15) DEFAULT NULL,
+  `STOCK` int(11) DEFAULT NULL,
+  `DID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PRODUCT`
+--
+
+LOCK TABLES `PRODUCT` WRITE;
+/*!40000 ALTER TABLE `PRODUCT` DISABLE KEYS */;
+INSERT INTO `PRODUCT` VALUES (1001,'snickers','food',100,5001);
+/*!40000 ALTER TABLE `PRODUCT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SUPERMARKET`
+--
+
+DROP TABLE IF EXISTS `SUPERMARKET`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SUPERMARKET` (
+  `PID` int(11) NOT NULL,
+  `P_NAME` varchar(15) DEFAULT NULL,
+  `P_TYPE` varchar(15) DEFAULT NULL,
+  `DID` int(11) DEFAULT NULL,
+  `COST` float DEFAULT NULL,
+  PRIMARY KEY (`PID`),
+  UNIQUE KEY `DID` (`DID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SUPERMARKET`
+--
+
+LOCK TABLES `SUPERMARKET` WRITE;
+/*!40000 ALTER TABLE `SUPERMARKET` DISABLE KEYS */;
+INSERT INTO `SUPERMARKET` VALUES (1001,'snickers','food',5001,35);
+/*!40000 ALTER TABLE `SUPERMARKET` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-21 21:26:13
